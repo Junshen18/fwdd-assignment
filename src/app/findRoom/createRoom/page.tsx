f2706970da3645125bf1e9a2ceeba7363cce8a1d
Junshen18 <junshenhere@gmail.com>
2024-08-06 02:11:06 +0800

    fix create room bug

--- src/app/findRoom/createRoom/page.tsx
+++ src/app/findRoom/createRoom/page.tsx
@@ -30,49 +30,48 @@ export default function CreateRoom() {
         throw new Error("Failed to generate lobby code");
       }
       const data = await response.json();
-      setLobbyCode(data.lobbyCode);
+      const newLobbyCode = data.lobbyCode;
+      setLobbyCode(newLobbyCode);
 
       const userName = localStorage.getItem("user_name");
       setUsername(userName);
-      console.log("Username: ", username, lobbyCode);
-      // if username and lobby code are set, join the room
-      if (username && lobbyCode) {
-        // const roomCode = await joinRoom(data.lobbyCode, username);
-
-        // get room id by using room code
-        const { data: room, error: roomError } = await supabase
-          .from("room")
-          .select("room_id, room_code, status")
-          .eq("room_code", lobbyCode)
-          .single();
-
-        //get user id
-        const user_id = parseInt(localStorage.getItem("user_id") || "0", 10);
-
-        // add player to room real time database
-        const { data: playerData, error: playerError } = await supabase
-          .from("player")
-          .insert([
-            {
-              user_id: user_id,
-              room_id: room?.room_id,
-              player_mp: 0,
-            },
-          ])
-          .select();
-
-        console.log("Joining room with code:", lobbyCode);
-
-        if (lobbyCode) {
-          router.push(`/lobby/${lobbyCode}`);
-        } else {
-          console.error("Room code is undefined or null");
-        }
-      } else {
-        console.error("Username or lobby code is not set");
-      }
+      console.log("Username: ", userName, "Lobby Code: ", newLobbyCode);
+      handleJoinRoom(newLobbyCode);
     } catch (error) {
       console.error("Error creating room:", error);
+    }
+  };
+
+  const handleJoinRoom = async (roomCode: string) => {
+    // get room id by using room code
+    if (roomCode) {
+      const { data: room, error: roomError } = await supabase
+        .from("room")
+        .select("room_id, room_code, status")
+        .eq("room_code", roomCode)
+        .single();
+
+      //get user id
+      const user_id = parseInt(localStorage.getItem("user_id") || "0", 10);
+
+      // add player to room real time database
+      const { error: playerError } = await supabase
+        .from("player")
+        .insert([
+          {
+            user_id: user_id,
+            room_id: room?.room_id,
+            player_mp: 0,
+          },
+        ])
+        .select();
+    }
+    console.log("Joining room with code:", roomCode);
+
+    if (roomCode) {
+      router.push(`/lobby/${roomCode}`);
+    } else {
+      console.error("Room code is undefined or null");
     }
   };
 
